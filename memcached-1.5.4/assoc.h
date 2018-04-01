b4de1336762d20b87508c1d984f5c48ea0d34e2e
steveLauwh <liuwenhong_steve@163.com>
2018-04-01 16:54:27 +0800

    哈希表头文件

--- memcached-1.5.4/assoc.h
+++ memcached-1.5.4/assoc.h
@@ -1,4 +1,6 @@
-/* associative array */
+// assoc.h 哈希表头文件
+
+/* associative array 关联数组(key, value)*/
 void assoc_init(const int hashpower_init);
 item *assoc_find(const char *key, const size_t nkey, const uint32_t hv);
 int assoc_insert(item *item, const uint32_t hv);
